64835354c33d4d0bdd760294d2f505947bf0a453
RobertoPacho <56565107+RobertoPacho@users.noreply.github.com>
2020-06-19 00:09:23 -0500

    Create Controlador_HistoriaClinicaTest.java

---
+++ src/test/java/ec/edu/ups/controlador/Controlador_HistoriaClinicaTest.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.controlador;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author hp
+ */
+public class Controlador_HistoriaClinicaTest {
+    
+    public Controlador_HistoriaClinicaTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of agregarPaciente method, of class Controlador_HistoriaClinica.
+     */
+    @Test
+    public void testAgregarPaciente() {
+        System.out.println("agregarPaciente");
+        Controlador_HistoriaClinica instance = new Controlador_HistoriaClinica();
+        instance.agregarPaciente();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of eliminarPaciente method, of class Controlador_HistoriaClinica.
+     */
+    @Test
+    public void testEliminarPaciente() {
+        System.out.println("eliminarPaciente");
+        Controlador_HistoriaClinica instance = new Controlador_HistoriaClinica();
+        instance.eliminarPaciente();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of buscarPaciente method, of class Controlador_HistoriaClinica.
+     */
+    @Test
+    public void testBuscarPaciente() {
+        System.out.println("buscarPaciente");
+        Controlador_HistoriaClinica instance = new Controlador_HistoriaClinica();
+        instance.buscarPaciente();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of actualizarPaciente method, of class Controlador_HistoriaClinica.
+     */
+    @Test
+    public void testActualizarPaciente() {
+        System.out.println("actualizarPaciente");
+        Controlador_HistoriaClinica instance = new Controlador_HistoriaClinica();
+        instance.actualizarPaciente();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
