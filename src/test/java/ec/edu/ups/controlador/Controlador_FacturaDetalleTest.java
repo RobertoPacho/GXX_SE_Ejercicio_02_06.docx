1c4bb1aec1b980d28d16d7753e242838a2abeb94
RobertoPacho <56565107+RobertoPacho@users.noreply.github.com>
2020-06-19 00:09:19 -0500

    Create Controlador_FacturaDetalleTest.java

---
+++ src/test/java/ec/edu/ups/controlador/Controlador_FacturaDetalleTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.controlador;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author hp
+ */
+public class Controlador_FacturaDetalleTest {
+    
+    public Controlador_FacturaDetalleTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of calcularTotal method, of class Controlador_FacturaDetalle.
+     */
+    @Test
+    public void testCalcularTotal() {
+        System.out.println("calcularTotal");
+        Controlador_FacturaDetalle instance = new Controlador_FacturaDetalle();
+        instance.calcularTotal();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
