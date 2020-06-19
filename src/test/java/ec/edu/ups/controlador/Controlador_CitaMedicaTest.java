09a53582113291d6b8b80c66217e48ab8275dbf3
RobertoPacho <56565107+RobertoPacho@users.noreply.github.com>
2020-06-19 00:08:57 -0500

    Create Controlador_CitaMedicaTest.java

---
+++ src/test/java/ec/edu/ups/controlador/Controlador_CitaMedicaTest.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.controlador;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author hp
+ */
+public class Controlador_CitaMedicaTest {
+    
+    public Controlador_CitaMedicaTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of agregarCita method, of class Controlador_CitaMedica.
+     */
+    @Test
+    public void testAgregarCita() {
+        System.out.println("agregarCita");
+        Controlador_CitaMedica instance = new Controlador_CitaMedica();
+        instance.agregarCita();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of eliminarCita method, of class Controlador_CitaMedica.
+     */
+    @Test
+    public void testEliminarCita() {
+        System.out.println("eliminarCita");
+        Controlador_CitaMedica instance = new Controlador_CitaMedica();
+        instance.eliminarCita();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of actualizarCita method, of class Controlador_CitaMedica.
+     */
+    @Test
+    public void testActualizarCita() {
+        System.out.println("actualizarCita");
+        Controlador_CitaMedica instance = new Controlador_CitaMedica();
+        instance.actualizarCita();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
