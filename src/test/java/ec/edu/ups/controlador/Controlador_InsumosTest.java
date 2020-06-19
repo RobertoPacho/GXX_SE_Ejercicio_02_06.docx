baccca610677af2d966e535dedf5d8dda31cb05f
RobertoPacho <56565107+RobertoPacho@users.noreply.github.com>
2020-06-19 00:09:26 -0500

    Create Controlador_InsumosTest.java

---
+++ src/test/java/ec/edu/ups/controlador/Controlador_InsumosTest.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.controlador;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author hp
+ */
+public class Controlador_InsumosTest {
+    
+    public Controlador_InsumosTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of agregarInsumos method, of class Controlador_Insumos.
+     */
+    @Test
+    public void testAgregarInsumos() {
+        System.out.println("agregarInsumos");
+        Controlador_Insumos instance = new Controlador_Insumos();
+        instance.agregarInsumos();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of actualizarInsumos method, of class Controlador_Insumos.
+     */
+    @Test
+    public void testActualizarInsumos() {
+        System.out.println("actualizarInsumos");
+        Controlador_Insumos instance = new Controlador_Insumos();
+        instance.actualizarInsumos();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of buscarInsumos method, of class Controlador_Insumos.
+     */
+    @Test
+    public void testBuscarInsumos() {
+        System.out.println("buscarInsumos");
+        Controlador_Insumos instance = new Controlador_Insumos();
+        instance.buscarInsumos();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
