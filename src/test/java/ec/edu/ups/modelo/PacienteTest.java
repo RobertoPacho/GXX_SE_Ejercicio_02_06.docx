d2fd315d54e1b5e3ef8d5eb46ae0425940470826
RobertoPacho <56565107+RobertoPacho@users.noreply.github.com>
2020-06-19 00:10:39 -0500

    Create PacienteTest.java

---
+++ src/test/java/ec/edu/ups/modelo/PacienteTest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.modelo;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author hp
+ */
+public class PacienteTest {
+    
+    public PacienteTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of calcularEdad method, of class Paciente.
+     */
+    @Test
+    public void testCalcularEdad() {
+        System.out.println("calcularEdad");
+        Paciente instance = new Paciente();
+        int expResult = 0;
+        int result = instance.calcularEdad();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
